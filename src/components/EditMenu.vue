24545fae3b2faa4c064387f9cdc67159de4b72f7
Yetrina Battad <hello@yetti.io>
2021-08-15 23:07:28 +1000

    fix(transition): adds transition for EditMenu component

--- src/components/EditMenu.vue
+++ src/components/EditMenu.vue
@@ -38,136 +38,139 @@
     </button>
 
     <!--
-                      Dropdown menu, show/hide based on menu state.
-
-                      Entering: "transition ease-out duration-100"
-                        From: "transform opacity-0 scale-95"
-                        To: "transform opacity-100 scale-100"
-                      Leaving: "transition ease-in duration-75"
-                        From: "transform opacity-100 scale-100"
-                        To: "transform opacity-0 scale-95"
-                    -->
-    <div
-      v-show="state.matches('visible')"
-      class="
-        mx-3
-        origin-top-right
-        absolute
-        right-7
-        top-0
-        w-48
-        mt-1
-        rounded-md
-        shadow-lg
-        z-10
-        bg-white
-        ring-1 ring-black ring-opacity-5
-        divide-y divide-gray-200
-        focus:outline-none
-      "
-      role="menu"
-      aria-orientation="vertical"
-      aria-labelledby="project-options-menu-0-button"
-      tabindex="-1"
+      Dropdown menu, show/hide based on menu state.
+    -->
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+      appear
     >
-      <div class="py-1" role="none">
-        <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-        <a
-          href="#"
-          class="text-gray-700 group flex items-center px-4 py-2 text-sm"
-          role="menuitem"
-          tabindex="-1"
-          id="project-options-menu-0-item-0"
-        >
-          <!-- Heroicon name: solid/pencil-alt -->
-          <svg
-            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
+      <div
+        v-show="state.matches('visible')"
+        class="
+          mx-3
+          origin-top-right
+          absolute
+          right-7
+          top-0
+          w-48
+          mt-1
+          rounded-md
+          shadow-lg
+          z-10
+          bg-white
+          ring-1 ring-black ring-opacity-5
+          divide-y divide-gray-200
+          focus:outline-none
+        "
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="project-options-menu-0-button"
+        tabindex="-1"
+      >
+        <div class="py-1" role="none">
+          <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
+          <a
+            href="#"
+            class="text-gray-700 group flex items-center px-4 py-2 text-sm"
+            role="menuitem"
+            tabindex="-1"
+            id="project-options-menu-0-item-0"
           >
-            <path
-              d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Edit
-        </a>
-        <a
-          href="#"
-          class="text-gray-700 group flex items-center px-4 py-2 text-sm"
-          role="menuitem"
-          tabindex="-1"
-          id="project-options-menu-0-item-1"
-        >
-          <!-- Heroicon name: solid/duplicate -->
-          <svg
-            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
+            <!-- Heroicon name: solid/pencil-alt -->
+            <svg
+              class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Edit
+          </a>
+          <a
+            href="#"
+            class="text-gray-700 group flex items-center px-4 py-2 text-sm"
+            role="menuitem"
+            tabindex="-1"
+            id="project-options-menu-0-item-1"
           >
-            <path
-              d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z"
-            />
-            <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z" />
-          </svg>
-          Duplicate
-        </a>
-        <a
-          href="#"
-          class="text-gray-700 group flex items-center px-4 py-2 text-sm"
-          role="menuitem"
-          tabindex="-1"
-          id="project-options-menu-0-item-2"
-        >
-          <!-- Heroicon name: solid/user-add -->
-          <svg
-            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
+            <!-- Heroicon name: solid/duplicate -->
+            <svg
+              class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z"
+              />
+              <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z" />
+            </svg>
+            Duplicate
+          </a>
+          <a
+            href="#"
+            class="text-gray-700 group flex items-center px-4 py-2 text-sm"
+            role="menuitem"
+            tabindex="-1"
+            id="project-options-menu-0-item-2"
           >
-            <path
-              d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"
-            />
-          </svg>
-          Share
-        </a>
+            <!-- Heroicon name: solid/user-add -->
+            <svg
+              class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"
+              />
+            </svg>
+            Share
+          </a>
+        </div>
+        <div class="py-1" role="none">
+          <a
+            href="#"
+            class="text-gray-700 group flex items-center px-4 py-2 text-sm"
+            role="menuitem"
+            tabindex="-1"
+            id="project-options-menu-0-item-3"
+          >
+            <!-- Heroicon name: solid/trash -->
+            <svg
+              class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Delete
+          </a>
+        </div>
       </div>
-      <div class="py-1" role="none">
-        <a
-          href="#"
-          class="text-gray-700 group flex items-center px-4 py-2 text-sm"
-          role="menuitem"
-          tabindex="-1"
-          id="project-options-menu-0-item-3"
-        >
-          <!-- Heroicon name: solid/trash -->
-          <svg
-            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Delete
-        </a>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
