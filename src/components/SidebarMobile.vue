2222763a6d5c86f3ff941922220474a95b2d85da
Yetrina Battad <hello@yetti.io>
2021-08-15 16:46:41 +1000

    feat(sidebar mobile): adds sidebar mobile component

---
+++ src/components/SidebarMobile.vue
@@ -0,0 +1,334 @@
+<template>
+  <div
+    v-if="props.visible"
+    class="fixed inset-0 flex z-40 lg:hidden"
+    role="dialog"
+    aria-modal="true"
+  >
+    <!--
+      Off-canvas menu overlay, show/hide based on off-canvas menu state.
+    -->
+    <transition
+      name="sidebar-overlay"
+      enter-active-class="transition-opacity ease-linear duration-300"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="transition-opacity ease-linear duration-300"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+      appear
+    >
+      <div
+        @click="closeSidebar()"
+        class="fixed inset-0 bg-gray-600 bg-opacity-75"
+        aria-hidden="true"
+      ></div>
+    </transition>
+
+    <!--
+      Off-canvas menu, show/hide based on off-canvas menu state.
+    -->
+    <transition
+      enter-active-class="transition ease-in-out duration-300 transform"
+      enter-from-class="-translate-x-full"
+      enter-to-class="translate-x-0"
+      leave-active-class="transition ease-in-out duration-300 transform"
+      leave-from-class="translate-x-0"
+      leave-to-class="-translate-x-full"
+      appear
+    >
+      <div
+        class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white"
+      >
+        <!--
+        Close button, show/hide based on off-canvas menu state.
+      -->
+        <transition
+          enter-active-class="ease-in-out duration-300"
+          enter-from-class="opacity-0"
+          enter-to-class="opacity-100"
+          leave-active-class="ease-in-out duration-300"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+          appear
+        >
+          <div class="absolute top-0 right-0 -mr-12 pt-2">
+            <button
+              @click="closeSidebar()"
+              class="
+                ml-1
+                flex
+                items-center
+                justify-center
+                h-10
+                w-10
+                rounded-full
+                focus:outline-none
+                focus:ring-2
+                focus:ring-inset
+                focus:ring-white
+              "
+            >
+              <span class="sr-only">Close sidebar</span>
+              <!-- Heroicon name: outline/x -->
+              <svg
+                class="h-6 w-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </transition>
+
+        <div class="flex-shrink-0 flex items-center px-4">
+          <logo />
+        </div>
+        <div class="mt-5 flex-1 h-0 overflow-y-auto">
+          <nav class="px-2">
+            <div class="space-y-1">
+              <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:text-gray-900 hover:bg-gray-50" -->
+              <a
+                href="#"
+                class="
+                  bg-gray-100
+                  text-gray-900
+                  group
+                  flex
+                  items-center
+                  px-2
+                  py-2
+                  text-base
+                  leading-5
+                  font-medium
+                  rounded-md
+                "
+                aria-current="page"
+              >
+                <!--
+                Heroicon name: outline/home
+
+                Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
+              -->
+                <svg
+                  class="text-gray-500 mr-3 flex-shrink-0 h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                  />
+                </svg>
+                Home
+              </a>
+
+              <a
+                href="#"
+                class="
+                  text-gray-600
+                  hover:text-gray-900 hover:bg-gray-50
+                  group
+                  flex
+                  items-center
+                  px-2
+                  py-2
+                  text-base
+                  leading-5
+                  font-medium
+                  rounded-md
+                "
+              >
+                <!-- Heroicon name: outline/view-list -->
+                <svg
+                  class="
+                    text-gray-400
+                    group-hover:text-gray-500
+                    mr-3
+                    flex-shrink-0
+                    h-6
+                    w-6
+                  "
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 10h16M4 14h16M4 18h16"
+                  />
+                </svg>
+                My tasks
+              </a>
+
+              <a
+                href="#"
+                class="
+                  text-gray-600
+                  hover:text-gray-900 hover:bg-gray-50
+                  group
+                  flex
+                  items-center
+                  px-2
+                  py-2
+                  text-base
+                  leading-5
+                  font-medium
+                  rounded-md
+                "
+              >
+                <!-- Heroicon name: outline/clock -->
+                <svg
+                  class="
+                    text-gray-400
+                    group-hover:text-gray-500
+                    mr-3
+                    flex-shrink-0
+                    h-6
+                    w-6
+                  "
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                Recent
+              </a>
+            </div>
+            <div class="mt-8">
+              <h3
+                class="
+                  px-3
+                  text-xs
+                  font-semibold
+                  text-gray-500
+                  uppercase
+                  tracking-wider
+                "
+                id="mobile-teams-headline"
+              >
+                Teams
+              </h3>
+              <div
+                class="mt-1 space-y-1"
+                role="group"
+                aria-labelledby="mobile-teams-headline"
+              >
+                <a
+                  href="#"
+                  class="
+                    group
+                    flex
+                    items-center
+                    px-3
+                    py-2
+                    text-base
+                    leading-5
+                    font-medium
+                    text-gray-600
+                    rounded-md
+                    hover:text-gray-900 hover:bg-gray-50
+                  "
+                >
+                  <span
+                    class="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="truncate"> Engineering </span>
+                </a>
+
+                <a
+                  href="#"
+                  class="
+                    group
+                    flex
+                    items-center
+                    px-3
+                    py-2
+                    text-base
+                    leading-5
+                    font-medium
+                    text-gray-600
+                    rounded-md
+                    hover:text-gray-900 hover:bg-gray-50
+                  "
+                >
+                  <span
+                    class="w-2.5 h-2.5 mr-4 bg-green-500 rounded-full"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="truncate"> Human Resources </span>
+                </a>
+
+                <a
+                  href="#"
+                  class="
+                    group
+                    flex
+                    items-center
+                    px-3
+                    py-2
+                    text-base
+                    leading-5
+                    font-medium
+                    text-gray-600
+                    rounded-md
+                    hover:text-gray-900 hover:bg-gray-50
+                  "
+                >
+                  <span
+                    class="w-2.5 h-2.5 mr-4 bg-yellow-500 rounded-full"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="truncate"> Customer Success </span>
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </transition>
+    <div class="flex-shrink-0 w-14" aria-hidden="true">
+      <!-- Dummy element to force sidebar to shrink to fit close icon -->
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps<{
+    visible: boolean
+  }>()
+
+  const emit = defineEmits<{
+    (e: 'closeSidebar'): void
+  }>()
+
+  const closeSidebar = () => {
+    emit('closeSidebar')
+  }
+</script>
