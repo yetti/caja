fd1900049cc86cd93525b1585eafef1c54e0357c
Yetrina Battad <hello@yetti.io>
2021-08-21 17:53:48 +1000

    chore(i18n): removes non-English transations

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -654,32 +654,6 @@
           </div>
           <div class="mt-4 flex sm:mt-0 sm:ml-4">
             <button
-              type="button"
-              class="
-                order-1
-                ml-3
-                inline-flex
-                items-center
-                px-4
-                py-2
-                border border-gray-300
-                shadow-sm
-                text-sm
-                font-medium
-                rounded-md
-                text-gray-700
-                bg-white
-                hover:bg-gray-50
-                focus:outline-none
-                focus:ring-2
-                focus:ring-offset-2
-                focus:ring-purple-500
-                sm:order-0 sm:ml-0
-              "
-            >
-              Share
-            </button>
-            <button
               @click="showSlideover()"
               type="button"
               class="
@@ -703,7 +677,7 @@
                 sm:order-1 sm:ml-3
               "
             >
-              Create
+              Add
             </button>
           </div>
         </div>
