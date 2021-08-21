9241cf2e3895ed7dbbeacf18c3ff6854e25c698c
Yetrina Battad <hello@yetti.io>
2021-08-21 20:34:35 +1000

    feat(functions): adds Netlify function to search bgg by name

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -69,91 +69,7 @@
                     <label
                       for="game-name"
                       class="block text-sm font-medium text-gray-700"
-                      >Game name</label
-                    >
-                    <div class="mt-1 flex rounded-md shadow-sm">
-                      <div
-                        class="
-                          relative
-                          flex
-                          items-stretch
-                          flex-grow
-                          focus-within:z-10
-                        "
-                      >
-                        <input
-                          type="text"
-                          name="game-name"
-                          id="game-name"
-                          class="
-                            focus:ring-indigo-500 focus:border-indigo-500
-                            block
-                            w-full
-                            rounded-none rounded-l-md
-                            sm:text-sm
-                            border-gray-300
-                          "
-                          placeholder="Carcassonne"
-                        />
-                      </div>
-                      <button
-                        type="button"
-                        class="
-                          -ml-px
-                          relative
-                          inline-flex
-                          items-center
-                          space-x-2
-                          px-4
-                          py-2
-                          border border-gray-300
-                          text-sm
-                          font-medium
-                          rounded-r-md
-                          text-gray-700
-                          bg-gray-50
-                          hover:bg-gray-100
-                          focus:outline-none
-                          focus:ring-1
-                          focus:ring-indigo-500
-                          focus:border-indigo-500
-                        "
-                      >
-                        <!-- Heroicon name: solid/search -->
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-5 w-5 text-gray-400"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                        <span>Search</span>
-                      </button>
-                    </div>
-                  </div>
-                  <div class="relative">
-                    <div
-                      class="absolute inset-0 flex items-center"
-                      aria-hidden="true"
-                    >
-                      <div class="w-full border-t border-gray-300"></div>
-                    </div>
-                    <div class="relative flex justify-center">
-                      <span class="px-2 bg-white text-sm text-gray-500">
-                        OR
-                      </span>
-                    </div>
-                  </div>
-                  <div>
-                    <label
-                      for="game-name"
-                      class="block text-sm font-medium text-gray-700"
-                      >Game name</label
+                      >Name</label
                     >
                     <div class="mt-1 flex rounded-md shadow-sm">
                       <div
@@ -231,7 +147,7 @@
                       tracking-wide
                     "
                   >
-                    Search Results
+                    Search Results (10)
                   </h3>
                   <ul
                     role="list"
