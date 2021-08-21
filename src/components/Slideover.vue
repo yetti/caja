527ad85356c60dcf9f31bc0d25bc48670c0cce99
Yetrina Battad <hello@yetti.io>
2021-08-21 10:13:14 +1000

    ci(netlify): updates Netlify config

--- src/components/Slideover.vue
+++ src/components/Slideover.vue
@@ -1,36 +1,38 @@
 <template>
-  <transition
-    enter-active-class="transform transition ease-in-out duration-500 sm:duration-700"
-    enter-from-class="translate-x-full"
-    enter-to-class="translate-x-0"
-    leave-active-class="transform transition ease-in-out duration-500 sm:duration-700"
-    leave-from-class="translate-x-0"
-    leave-to-class="translate-x-full"
-    appear
-  >
-    <div
-      v-show="props.visible"
-      class="fixed inset-0 overflow-hidden z-50 h-screen"
-      aria-labelledby="slide-over-title"
-      role="dialog"
-      aria-modal="true"
+  <client-only>
+    <transition
+      enter-active-class="transform transition ease-in-out duration-500 sm:duration-700"
+      enter-from-class="translate-x-full"
+      enter-to-class="translate-x-0"
+      leave-active-class="transform transition ease-in-out duration-500 sm:duration-700"
+      leave-from-class="translate-x-0"
+      leave-to-class="translate-x-full"
+      appear
     >
-      <div class="absolute inset-0 overflow-hidden">
-        <!-- Background overlay, show/hide based on slide-over state. -->
-        <div class="absolute inset-0" aria-hidden="true">
-          <div
-            class="fixed inset-y-0 sm:pl-16 max-w-full right-0 flex"
-            ref="target"
-          >
-            <!--
+      <div
+        v-show="props.visible"
+        class="fixed inset-0 overflow-hidden z-50 h-screen"
+        aria-labelledby="slide-over-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        <div class="absolute inset-0 overflow-hidden">
+          <!-- Background overlay, show/hide based on slide-over state. -->
+          <div class="absolute inset-0" aria-hidden="true">
+            <div
+              class="fixed inset-y-0 sm:pl-16 max-w-full right-0 flex"
+              ref="target"
+            >
+              <!--
             Slide-over panel, show/hide based on slide-over state.
             -->
-            <slot></slot>
+              <slot></slot>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </transition>
+    </transition>
+  </client-only>
 </template>
 
 <script setup lang="ts">
