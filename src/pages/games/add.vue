15415cbdc3964f0286838c5669c19c22389c6204
Yetrina Battad <hello@yetti.io>
2021-08-28 08:07:31 +1000

    feat(games): integrates game search component into slideout

--- src/pages/games/add.vue
+++ src/pages/games/add.vue
@@ -12,40 +12,6 @@
         <div class="flex-1 flex flex-col justify-between">
           <div class="px-4 sm:px-6">
             <GameSearch></GameSearch>
-            <div class="relative mt-10 mb-10">
-              <div
-                class="absolute inset-0 flex items-center"
-                aria-hidden="true"
-              >
-                <div class="w-full border-t border-gray-300"></div>
-              </div>
-              <div class="relative flex justify-center">
-                <button
-                  type="button"
-                  class="
-                    inline-flex
-                    items-center
-                    shadow-sm
-                    px-4
-                    py-1.5
-                    border border-gray-300
-                    text-sm
-                    leading-5
-                    font-medium
-                    rounded-full
-                    text-gray-700
-                    bg-white
-                    hover:bg-gray-50
-                    focus:outline-none
-                    focus:ring-2
-                    focus:ring-offset-2
-                    focus:ring-indigo-500
-                  "
-                >
-                  <span>See all results</span>
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
